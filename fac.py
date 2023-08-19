557979536f3b5a8f4afcefcc048058385806b0df
gaurav sagar <142651151+gaurav-sagar17@users.noreply.github.com>
2023-08-19 13:20:19 +0530

    Add files via upload

---
+++ fac.py
@@ -0,0 +1,6 @@
+a = int(input('enter the number you want to find the factorial of :'))
+f =1
+for i in range(1,a+1):
+                    f *= i
+                    
+print("the factorial id : " , f)
